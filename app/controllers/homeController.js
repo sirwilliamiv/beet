750ebe1ca1462fb607d6666b3013f02a441c4d89
sirwilliamiv <wjconnolly2@gmail.com>
2017-02-07 14:42:29 -0600

    audio firing on click

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -2,26 +2,48 @@ app.controller('homeCtrl', function($http, $scope) {
   console.log("homeCtrl")
 
   const sounds = {
-    Kick: '/assets/audio/beet/BD.mp3',
-    Snare: '/assets/audio/beet/SN.mp3',
-    HiHat: '/assets/audio/beet/HH.mp3',
-    OHiHat: '/assets/audio/beet/openHH.mp3'
+    Kick: 'BD.mp3',
+    Snare: 'SN.mp3',
+    HiHat: 'HH.mp3',
+    OHiHat: 'openHH.mp3'
   };
   const grid = 16;
+  $scope.play =(beat)=> {
 
+    for(k in sounds){
+      if(k === beat.name){
+
+        beat.sample.play()
+
+      }
+    }
+    // beat.sample.play((id)=> {
+    //   debugger
+    //   console.log("soundid", id)
+    // })
+
+    console.log("i should play sound from ", beat.name)
+//     sound.play(function(soundId){
+//     // do what you want with soundId
+// });
+
+  }
   $scope.sing = () => {
     let rows = []
     for (file in sounds) {
+
       //add sample
       let sample = new Howl({
-          src: [`'sounds' + '.'+ file`],
+          src: [`/assets/audio/beet/${sounds[file]}`],
           volume: 1.0
         })
+
         //combine sample and  name
       let track = {
           name: file,
           sample: sample
         }
+
         // add 16 tracks per row
       for (var i = 0; i < grid; i++) {
         rows.push(track)
@@ -30,26 +52,8 @@ app.controller('homeCtrl', function($http, $scope) {
     // return $scope.rows
     console.log(rows)
     $scope.rows = rows
-    debugger
-  //   let r1 =''
-  //   let r2 =''
-  //   let r3 =''
-  //   let r4 =''
-  //   for (var i = 0; i < rows.length; i++) {
-  //     if (i <= 15) {
-  //       $scope.r1 = i
-  //     }
-  //     if (i > 15 && i <= 31) {
-  //       $scope.r2 = i
-  //     }
-  //     if (i > 31 && i <= 47) {
-  //       $scope.r3 = i
-  //     }
-  //     if (i > 47) {
-  //       $scope.r4 = i
-  //     }
-  //     debugger
-  // }  //end rows for loop
+
+
 }
 
 
