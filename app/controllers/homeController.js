8b1134c8391644a133495067491e21464829a821
sirwilliamiv <wjconnolly2@gmail.com>
2017-02-08 09:42:23 -0600

    able to load new object to dom

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -2,59 +2,54 @@ app.controller('homeCtrl', function($http, $scope) {
   console.log("homeCtrl")
 
   const sounds = {
-    Kick: 'BD.mp3',
-    Snare: 'SN.mp3',
-    HiHat: 'HH.mp3',
-    OHiHat: 'openHH.mp3'
+    kick: 'BD.mp3',
+    snare: 'SN.mp3',
+    hihat: 'HH.mp3',
+    ohihat: 'openHH.mp3'
   };
   const grid = 16;
-  $scope.play =(beat)=> {
 
-    for(k in sounds){
-      if(k === beat.name){
 
-        beat.sample.play()
 
-      }
-    }
-    // beat.sample.play((id)=> {
-    //   debugger
-    //   console.log("soundid", id)
-    // })
-
-    console.log("i should play sound from ", beat.name)
-//     sound.play(function(soundId){
-//     // do what you want with soundId
-// });
-
-  }
   $scope.sing = () => {
-    let rows = []
-    for (file in sounds) {
-
-      //add sample
-      let sample = new Howl({
-          src: [`/assets/audio/beet/${sounds[file]}`],
-          volume: 1.0
-        })
-
-        //combine sample and  name
-      let track = {
-          name: file,
-          sample: sample
+      let instruments = {
+          kick: [],
+          snare: [],
+          hihat: [],
+          openhihat: []
         }
+        //adding files to instruments object
+      for (name in sounds) {
+        let sample = new Howl({
+            src: [`/assets/audio/beet/${sounds[name]}`],
+            volume: 1.0
+          })
+          //combine sample and  name
+        let track = {
+            name: name,
+            sample: sample
+          }
+          // add 16 tracks per row
+        for (var i = 0; i < grid; i++) {
+          //add tracks to instruments object by name
+          instruments[name].push(track)
+        } // end grid for loop
+      } //end sounds for in loop
+      console.log(instruments)
+      return $scope.instruments = instruments
 
-        // add 16 tracks per row
-      for (var i = 0; i < grid; i++) {
-        rows.push(track)
-      }
-    } //end for in loop
-    // return $scope.rows
-    console.log(rows)
-    $scope.rows = rows
+    }
 
 
-}
-
+  //   ///play the sound
+  // $scope.play = (beat) => {
+  //   //match beat to play
+  //   for (k in sounds) {
+  //     if (k === beat.name) {
+  //       beat.sample.play()
+  //     }
+  //   }
+  //   console.log("i should play sound from ", beat.name)
+  // }
 
 });
