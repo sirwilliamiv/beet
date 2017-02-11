7180c2987c717679db9be857397263737abae621
sirwilliamiv <wjconnolly2@gmail.com>
2017-02-11 15:20:16 -0600

    need to remove sample.src before sending to firebase to avert ctypeError

--- app/controllers/mainBeetController.js
+++ app/controllers/mainBeetController.js
@@ -26,7 +26,7 @@ app.controller('mainCtrl', function($http, $scope, $timeout, $interval) {
         //adding files to instruments object
       for (name in sounds) {
         let sample = new Howl({
-            src: [`/assets/audio/beet/${sounds[name]}`],
+            // src: [`/assets/audio/beet/${sounds[name]}`],
             volume: 0.8,
             html5: true
           })
@@ -87,46 +87,21 @@ app.controller('mainCtrl', function($http, $scope, $timeout, $interval) {
       }, bpm * i);
     }
     //save pattern and convert to object
-    //http://stackoverflow.com/questions/32002176/how-to-convert-array-to-object-in-javascript
-    // $scope.save = function() {
-    //   console.log("save")
-    //   debugger
-    //   let beetCopy = Object.assign({}, $scope.instruments)
-    //   let valueList = []
-    //     // THIS DOESNT WORK ****************
-    //     //LEARN WHAT THIS DOES http://stackoverflow.com/questions/4215737/convert-array-to-object
-    //   for (var i = 0; i < beetCopy.kick.length; i++) {
-    //     valueList.push(beetCopy.kick[i].value)
 
-  //   } // end for loop
+  $scope.save = function() {
+    console.log("save")
+    //*** NEED TO REMOVE SRC LINE FROM SAMPLE OBJECT BEFORE SENDING****
+debugger
+    // let beetCopy = Object.assign({}, $scope.instruments)
+    $scope.saveThisBeet($scope.instruments)
 
-  //   //returns <-- array [true,false,false...etc ] length is 16
-  //   return function toObject(valueList) {
-  //     var rv = {};
-  //     for (var i = 0; i < valueList.length; ++i)
-  //       if (valueList[i] !== undefined) rv[i] = valueList[i];
-  //     debugger
-  //     console.log("rv",rv)
-  //     return rv; //object with 16 keys??
-  //   } //end toObject function
-  // } //end save function
-
-  // *********************************************************
+  } //end save function
 
 
-  //   let savedBeetForFB = toObject(beetCopy.kick)
-  //   debugger
-
-
-  //   // console.log("beetCopy", beetCopy)
-  //   $scope.saveThisBeet(savedBeetForFB)
-  //   debugger
-  // }
-
-  // // send savedBeet to firebase
-  // $scope.saveThisBeet = function(savedBeet) {
-  //   $http.post(`https://beet-35be8.firebaseio.com/userBeets.json`, JSON.stringify(savedBeet))
-  // }
+  // send savedBeet to firebase
+  $scope.saveThisBeet = function(savedBeet) {
+    $http.post(`https://beet-35be8.firebaseio.com/userBeets.json`, JSON.stringify(savedBeet))
+  }
 
 
 
